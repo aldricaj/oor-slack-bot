e4f7c19007ee734c66318f991ad5efd9b93bd68b
Andrew Aldrich <aaldrich@andrew-aldrich.me>
2018-01-23 01:57:35 -0500

    initial testing commit

---
+++ main-app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import request
+from gevent.wsgi import WSGIServer
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/slackapi/linkfor', methods=['POST'])
+def handle_linkfor():
+    data = request.form 
+    print(data.text)
+
+http_server = WSGIServer(('', 80), app)
+http_server.serve_forever()
